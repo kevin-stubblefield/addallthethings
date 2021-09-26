59deda87711fc3d7e62158963c0e7b3dbed46e34
Kevin Stubblefield <kevin@stubblefield.io>
2021-09-25 22:39:48 -0500

    Add docker pg script

---
+++ runpg.sh
@@ -0,0 +1,6 @@
+ROOT_DIR=$(pwd)
+PG_DATA="$ROOT_DIR/data/postgres"
+
+mkdir -p "$PG_DATA"
+
+docker run -d --name addallthethings -p 5432:5432 -e POSTGRES_USER=addallthethings -e POSTGRES_PASSWORD=localhost -e POSTGRES_DB=addallthethings_db -e PGDATA=/var/lib/postgresql/data -v $PG_DATA:/var/lib/postgresql/data postgres:11
