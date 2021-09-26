3545fc5a44c04c1fec43c35e31a71b77ac34602b
Kevin Stubblefield <kevin@stubblefield.io>
2021-09-25 23:05:23 -0500

    modify runpg.sh to start or create pg container

--- runpg.sh
+++ runpg.sh
@@ -3,4 +3,5 @@ PG_DATA="$ROOT_DIR/data/postgres"
 
 mkdir -p "$PG_DATA"
 
-docker run -d --name addallthethings -p 5432:5432 -e POSTGRES_USER=addallthethings -e POSTGRES_PASSWORD=localhost -e POSTGRES_DB=addallthethings_db -e PGDATA=/var/lib/postgresql/data -v $PG_DATA:/var/lib/postgresql/data postgres:11
+
+docker start addallthethings || docker run -d --name addallthethings -p 5432:5432 -e POSTGRES_USER=addallthethings -e POSTGRES_PASSWORD=localhost -e POSTGRES_DB=addallthethings_db -e PGDATA=/var/lib/postgresql/data -v $PG_DATA:/var/lib/postgresql/data postgres:11
