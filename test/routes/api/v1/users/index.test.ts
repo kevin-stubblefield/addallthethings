e22e51534dba29400d211192eedbad8f08e855b5
Kevin Stubblefield <kevin@stubblefield.io>
2021-10-09 18:25:27 -0500

    get users to 100% tested

--- test/routes/api/v1/users/index.test.ts
+++ test/routes/api/v1/users/index.test.ts
@@ -1,6 +1,6 @@
 import { build } from '../../../../helper';
 
-describe('create discord user route', () => {
+describe('user routes', () => {
   const app = build();
 
   test('should create a user with discord information', async () => {
@@ -18,11 +18,53 @@ describe('create discord user route', () => {
     });
 
     expect(res.statusCode).toBe(201);
-    expect(res.json().discord_username).toBe(requestPayload.discord_username);
-    expect(res.json().discord_discriminator).toBe(
-      requestPayload.discord_discriminator
-    );
-    expect(res.json().discord_tag).toBe(requestPayload.discord_tag);
-    expect(res.json().discord_user_id).toBe(requestPayload.discord_user_id);
+    expect(res.json()).toMatchObject(requestPayload);
+  });
+
+  test('should return error if new user already exists', async () => {
+    const requestPayload = {
+      discord_username: 'test_username',
+      discord_discriminator: '1234',
+      discord_tag: 'test_username#1234',
+      discord_user_id: '1235468436206186564',
+    };
+
+    let res = await app.inject({
+      url: '/api/v1/users',
+      method: 'POST',
+      payload: requestPayload,
+    });
+
+    res = await app.inject({
+      url: '/api/v1/users',
+      method: 'POST',
+      payload: requestPayload,
+    });
+
+    expect(res.statusCode).toBe(409);
+  });
+
+  test('should retrieve a user by their discord id', async () => {
+    // create user to be retrieved
+    const expected = {
+      discord_username: 'test_username',
+      discord_discriminator: '1234',
+      discord_tag: 'test_username#1234',
+      discord_user_id: '1235468436206186564',
+    };
+
+    await app.inject({
+      url: '/api/v1/users',
+      method: 'POST',
+      payload: expected,
+    });
+
+    const res = await app.inject({
+      url: `/api/v1/users/${expected.discord_user_id}`,
+      method: 'GET',
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toMatchObject(expected);
   });
 });
