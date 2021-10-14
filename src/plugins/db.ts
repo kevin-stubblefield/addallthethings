117e2e801dba16a54df470dcefd3dd8904ff3323
Kevin Stubblefield <kevin@stubblefield.io>
2021-10-13 19:15:35 -0500

    Fix open async handle

--- src/plugins/db.ts
+++ src/plugins/db.ts
@@ -2,28 +2,24 @@ import fp from 'fastify-plugin';
 import knex, { Knex } from 'knex';
 import knexConfig from '../../knexfile';
 
-async function runMigrations(config: Knex.Config): Promise<any> {
-  return await knex(config).migrate.latest();
+async function runMigrations(db: Knex): Promise<any> {
+  return await db.migrate.latest();
 }
 
 export default fp(
   async function (fastify, opts) {
-    const db = knex({
-      client: 'pg',
-      connection: fastify.config.postgresUri,
-    });
-
-    fastify.decorate('db', db).addHook('onClose', async (instance, done) => {
-      await db.destroy();
-      done();
-    });
-
     const dbConfig =
       process.env.NODE_ENV || 'development' === 'development'
         ? knexConfig.development
         : knexConfig.production;
 
-    const migrationResults = await runMigrations(dbConfig);
+    const db = knex(dbConfig);
+
+    fastify.decorate('db', db).addHook('onClose', async (instance) => {
+      await db.destroy();
+    });
+
+    const migrationResults = await runMigrations(db);
 
     if (migrationResults.length > 0) {
       fastify.log.info({
